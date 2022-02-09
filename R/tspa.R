f6219ac93f3df8286220355dc2d40250f46be07a
Winnie Wing-Yee Tse <wingyeet@usc.edu>
2022-02-08 19:51:50 -0800

    Made some changes to Yixiao's function

--- R/tspa.R
+++ R/tspa.R
@@ -7,21 +7,21 @@ tspa <- function(model, data, reliability = NULL) {
 
 # model-building code w/o test on actual data
 tspa <- function(model, data, reliability = NULL) {
-  models <- strsplit(model, split = "~")
-  ind <- models[0]
-  dem <- models[1]
-  ev1 <- data['ev1']
-  ev2 <- data['ev2']
-  fs_ind <- data['fs_ind']
-  fs_dem <- data['fs_dem']
-  tspaModel <- ind + ' =~ 1 * ' + fs_ind + '\n' +
-    dem + ' =~ 1 * ' + fs_dem + '\n' +
-    fs_ind + ' ~~ ' + ev1 + ' * ' + fs_ind + '\n' +
-    fs_dem + ' ~~ ' + ev2 + ' * ' + fs_dem + '\n' +
-    ind + ' ~~ v1 * ' + ind + '\n' +
-    dem + ' ~~ v2 * ' + dem + '\n' +
+  models <- strsplit(model, split = " ~ ")
+  x <- models[[1]][2]
+  y <- models[[1]][1]
+  fs_x <- colnames(data)[1]
+  fs_y <- colnames(data)[2]
+  tspaModel <- x + ' =~ 1 * ' + fs_x + '\n' +
+    y + ' =~ 1 * ' + fs_y + '\n' +
+    fs_x + ' ~~ ' + ev1 + ' * ' + fs_x + '\n' +
+    fs_y + ' ~~ ' + ev2 + ' * ' + fs_y + '\n' +
+    x + ' ~~ v1 * ' + x + '\n' +
+    y + ' ~~ v2 * ' + y + '\n' +
     model + '\n' +
-    'v1 == ' + toString(reliability[0]) + ' / ' + toString(1 - reliability[0]) + ' * ' + ev1 + '\n' +
-    'v2 == ' + toString(reliability[1]) + ' / ' + toString(1 - reliability[1]) + ' * ' + ev2 + '\n'
-  return (tspaModel)
+    'v1 == ' + toString(reliability[1]) + ' / ' + toString(1 - reliability[1]) + ' * ' + ev1 + '\n' +
+    'v2 == ' + toString(reliability[2]) + ' / ' + toString(1 - reliability[2]) + ' * ' + ev2 + '\n'
+  tspa_fit <- sem(model = tspaModel,
+                  data  = data)
+  return (tspa_fit)
 }
