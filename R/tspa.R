94b6b83abf15414514bae53ecbbf243432d5277f
Yixiao Li <likather@usc.edu>
2022-02-12 09:34:02 -0800

    paste string revised

--- R/tspa.R
+++ R/tspa.R
@@ -12,15 +12,15 @@ tspa <- function(model, data, reliability = NULL) {
   y <- models[[1]][1]
   fs_x <- colnames(data)[1]
   fs_y <- colnames(data)[2]
-  tspaModel <- x + ' =~ 1 * ' + fs_x + '\n' +
-    y + ' =~ 1 * ' + fs_y + '\n' +
-    fs_x + ' ~~ ' + ev1 + ' * ' + fs_x + '\n' +
-    fs_y + ' ~~ ' + ev2 + ' * ' + fs_y + '\n' +
-    x + ' ~~ v1 * ' + x + '\n' +
-    y + ' ~~ v2 * ' + y + '\n' +
-    model + '\n' +
-    'v1 == ' + toString(reliability[1]) + ' / ' + toString(1 - reliability[1]) + ' * ' + ev1 + '\n' +
-    'v2 == ' + toString(reliability[2]) + ' / ' + toString(1 - reliability[2]) + ' * ' + ev2 + '\n'
+  tspaModel <- paste0(x, ' =~ 1 * ', fs_x, '\n',
+    y, ' =~ 1 * ', fs_y, '\n',
+    fs_x, ' ~~ ev1 * ', fs_x, '\n',
+    fs_y, ' ~~ ev2 * ', fs_y, '\n',
+    x, ' ~~ v1 * ', x, '\n',
+    y, ' ~~ v2 * ', y, '\n',
+    model, '\n',
+    'v1 == ', toString(reliability[1]), ' / ', toString(1 - reliability[1]), ' * ev1\n',
+    'v2 == ', toString(reliability[2]), ' / ', toString(1 - reliability[2]), ' * ev2\n')
   tspa_fit <- sem(model = tspaModel,
                   data  = data)
   return (tspa_fit)
