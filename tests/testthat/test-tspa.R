05043f82c2a9967cf72fadd98237b1fde80f8f45
Winnie Wing-Yee Tse <wingyeet@usc.edu>
2022-02-15 15:22:42 -0800

    Fixed the test example in test-tspa.R #3

--- tests/testthat/test-tspa.R
+++ tests/testthat/test-tspa.R
@@ -7,6 +7,7 @@ my_cfa <- '
 cfa_fit <- cfa(model = my_cfa,
                data  = PoliticalDemocracy)
 fs_dat <- lavPredict(cfa_fit, se = "standard")
+colnames(fs_dat) <- c("fs_ind60", "fs_dem60")
 
 test_that("example in the vignette", {
   expect_equal(round(
