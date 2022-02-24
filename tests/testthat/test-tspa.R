76ed46fb6b1d21c767d166645b43f38a341ffb14
Winnie Wing-Yee Tse <wingyeet@usc.edu>
2022-02-24 14:54:04 -0800

    Added a 3-variable example #2

--- tests/testthat/test-tspa.R
+++ tests/testthat/test-tspa.R
@@ -1,4 +1,5 @@
 library(lavaan)
+# example 1
 my_cfa <- '
    # latent variables
      ind60 =~ x1 + x2 + x3
@@ -9,6 +10,30 @@ cfa_fit <- cfa(model = my_cfa,
 fs_dat <- lavPredict(cfa_fit, se = "standard")
 colnames(fs_dat) <- c("fs_ind60", "fs_dem60")
 
+# example 2
+cfa_3var <- '
+   # latent variables
+     ind60 =~ x1 + x2 + x3
+     dem60 =~ y1 + y2 + y3 + y4
+     dem65 =~ y5 + y6 + y7 + y8
+   # residual correlations
+     y1 ~~ y5
+     y2 ~~ y4 + y6
+     y3 ~~ y7
+     y4 ~~ y8
+     y6 ~~ y8
+'
+cfa_3var_fit <- cfa(model = cfa_3var,
+                    data  = PoliticalDemocracy)
+fs_3var_dat <- lavPredict(cfa_3var_fit, se = "standard")
+colnames(fs_3var_dat) <- c("fs_ind60", "fs_dem60", "fs_dem65")
+# tspa(model = "dem60 ~ ind60
+#               dem65 ~ ind60 + dem60",
+#      data = fs_dat,
+#      reliability = c(ind60 = 0.9968282, dem60 = 0.8503460, dem65 = 0.8430526))
+
+
+# tests
 test_that("example in the vignette", {
   expect_equal(round(
     parameterEstimates(
