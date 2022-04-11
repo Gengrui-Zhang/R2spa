395f593af1f23154c330b6361854aa1b0e35bf97
Hok Chio (Mark) Lai <marklhc@gmail.com>
2022-04-11 11:12:12 -0700

    Add se. Close #11

--- R/get_fscore.R
+++ R/get_fscore.R
@@ -3,6 +3,10 @@
 #' @param model An optional string specifying the measurement model
 #'              in \code{lavaan} syntax.
 #'              See \code{\link[lavaan]{model.syntax}} for more information.
+#' @param ... additional arguments passed to \code{\link[lavaan]{cfa}}. See
+#'            \code{\link[lavaan]{lavOptions}} for a complete list.
+#' @return A data frame containing the factor scores (prefaced with "fs_"),
+#'         the standard errors, and the corresponding reliability.
 #' @examples
 #' library(lavaan)
 #' get_fs(PoliticalDemocracy[c("x1", "x2", "x3")])
@@ -19,8 +23,9 @@ get_fs <- function(data, model = NULL, ...) {
   # Need to allow for additional options
   fscore <- lavPredict(fit, se = "standard")
   fscore_se <- attr(fscore, "se")[[1]]
-  rho <- 1 - fscore_se^2 / diag(psi)
+  fscore_rho <- 1 - fscore_se^2 / diag(psi)
   colnames(fscore) <- paste0("fs_", colnames(fscore))
-  colnames(rho) <- paste0("fs_", colnames(rho), "_se")
-  cbind(as.data.frame(fscore), rho)
+  colnames(fscore_se) <- paste0("fs_", colnames(fscore_se), "_se")
+  colnames(fscore_rho) <- paste0("fs_", colnames(fscore_rho), "_rel")
+  cbind(as.data.frame(fscore), fscore_se, fscore_rho)
 }
