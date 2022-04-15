35f60371ab1fe79b27fbd05f5cf7257d87115079
Hok Chio (Mark) Lai <marklhc@gmail.com>
2022-04-14 22:55:48 -0700

    Initial implement of multi-group in `get_fs()` #4

--- R/get_fscore.R
+++ R/get_fscore.R
@@ -10,22 +10,42 @@
 #' @examples
 #' library(lavaan)
 #' get_fs(PoliticalDemocracy[c("x1", "x2", "x3")])
+#'
+#' # Multiple-group
+#' hs_model <- ' visual  =~ x1 + x2 + x3 '
+#' fit <- cfa(hs_model,
+#'            data = HolzingerSwineford1939,
+#'            group = "school")
+#' get_fs(HolzingerSwineford1939, hs_model, group = "school")
 
-get_fs <- function(data, model = NULL, ...) {
+get_fs <- function(data, model = NULL, group = NULL, ...) {
   if (!is.data.frame(data)) data <- as.data.frame(data)
   ind_names <- colnames(data)
   if (is.null(model)) {
     model <- paste("f1 =~",
                    paste(ind_names, collapse = " + "))
   }
-  fit <- cfa(model, data = data, ...)
-  psi <- lavInspect(fit, what = "est")$psi
-  # Need to allow for additional options
+  fit <- cfa(model, data = data, group = group, ...)
+  est <- lavInspect(fit, what = "est")
   fscore <- lavPredict(fit, se = "standard")
-  fscore_se <- attr(fscore, "se")[[1]]
-  fscore_rho <- 1 - fscore_se^2 / diag(psi)
-  colnames(fscore) <- paste0("fs_", colnames(fscore))
-  colnames(fscore_se) <- paste0("fs_", colnames(fscore_se), "_se")
-  colnames(fscore_rho) <- paste0("fs_", colnames(fscore_rho), "_rel")
-  cbind(as.data.frame(fscore), fscore_se, fscore_rho)
+  fscore_se <- attr(fscore, "se")
+  if (is.null(group)) {
+    augment_fs(est, fscore, fscore_se[[1]])
+  } else {
+    fs_list <- lapply(seq_along(est), function(i) {
+      fs_dat <- augment_fs(est[[i]], fscore[[i]], fscore_se[[i]])
+      fs_dat[[group]] <- names(est[i])
+      fs_dat
+    })
+    do.call(rbind, fs_list)
+  }
+}
+
+augment_fs <- function(est, fs, fs_se) {
+  psi <- est$psi
+  fs_rho <- 1 - fs_se^2 / diag(psi)
+  colnames(fs) <- paste0("fs_", colnames(fs))
+  colnames(fs_se) <- paste0("fs_", colnames(fs_se), "_se")
+  colnames(fs_rho) <- paste0("fs_", colnames(fs_rho), "_rel")
+  cbind(as.data.frame(fs), fs_se, fs_rho)
 }
