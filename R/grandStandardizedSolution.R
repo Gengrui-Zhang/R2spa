84c1568a68367c4424bf620dc5b8848d598c1b7e
Winnie Wing-Yee Tse <wingyeet@usc.edu>
2022-09-16 09:14:07 -0700

    Added wald test z, pvalue, ci

---
+++ R/grandStandardizedSolution.R
@@ -0,0 +1,113 @@
+#' Grand Standardized Solution
+#'
+#' Grand standardized solution of a two-stage path analysis model.
+#'
+#' @param model_list A list of string variable describing the structural path
+#'                   model, in \code{lavaan} syntax
+#' @param se A Boolean variable. If TRUE, standard errors for the grand
+#'                   standardized parameters will be computed.
+#' @param acov_par An asymptotic variance-covariance matrix for a fitted
+#'                 model object.
+#' @param free_list A list of model matrices that indicate the position of
+#'                  the free parameters in the parameter vector.
+#' @return A matrix of the standardized model parameters and standard errors.
+#' @export
+#'
+#' @example
+#' library(lavaan)
+#'
+#' myModel <- '
+#'    # latent variables
+#'      ind60 =~ x1 + x2 + x3
+#'      dem60 =~ y1 + y2 + y3 + y4
+#'    # regressions
+#'      dem60 ~ ind60
+#' '
+#' fit <- sem(model = myModel,
+#'           data  = PoliticalDemocracy)
+#'
+#' lavInspect(fit, what = "cov.lv")  # lavaan
+#' veta(fit@Model@GLIST$beta, fit@Model@GLIST$psi)
+#'
+#'
+#' std_beta_est(lavInspect(fit, what = "est"),
+#'              free_list = lavInspect(fit, what = "free"),
+#'              est = c(1.5, 0.449, 3.453))
+#'
+#' grandStardardizedSolution(lavInspect(fit, what = "est"), se = TRUE,
+#'                           acov_par = vcov(fit),
+#'                           free_list = lavInspect(fit, what = "free"))
+
+grandStardardizedSolution <- function(model_list, se = TRUE, acov_par = NULL,
+                                      free_list = NULL) {
+  tmp_std_beta <- std_beta_est(model_list)
+  out <- list(std_beta = tmp_std_beta[which(tmp_std_beta != 0)])
+  if (se) {
+    free_beta_psi <- free_list[c("beta", "psi")]
+    est <- .combine_est(model_list[c("beta", "psi")],
+                        free = free_beta_psi)
+    jac <- lav_func_jacobian_complex(function(x)
+      std_beta_est(model_list, free_list = free_list, est = x),
+      x = est)
+    pos_beta_psi <- .combine_est(free_beta_psi, free = free_beta_psi)
+    acov_beta_psi <- acov_par[pos_beta_psi, pos_beta_psi]
+    tmp_acov_std_beta <- jac %*% acov_beta_psi %*% t(jac)
+    out$acov_std_beta <- tmp_acov_std_beta[which(tmp_acov_std_beta != 0)]
+    out$z <- out$std_beta / out$acov_std_beta
+    out$pvalue <- 2 * (1 - pnorm(abs(out$z)))
+    ci <- out$std_beta + out$acov_std_beta *
+      qnorm(c((1 - .95)/2, 1 - (1 - .95)/2))
+    out$ci.lower <- ci[1]
+    out$ci.upper <- ci[2]
+  }
+  as.data.frame(out)
+}
+
+# Latent variances
+veta <- function(beta, psi, gamma = NULL, cov_x = NULL) {
+  inv_Imb <- solve(diag(nrow = nrow(beta)) - beta)
+  if (!is.null(gamma) && !is.null(cov_x)) {
+    psi_plus_vgammax <- psi + gamma %*% cov_x %*% t(gamma)
+  } else {
+    psi_plus_vgammax <- psi
+  }
+  inv_Imb %*% psi_plus_vgammax %*% t(inv_Imb)
+}
+
+.fill_matrix_list <- function(mod, free, est) {
+  start_idx <- 0
+  for (m in seq_along(mod)) {
+    len_m <- sum(free[[m]] != 0)
+    if (len_m == 0) next
+    m_idx <- start_idx + seq_len(len_m)
+    mod[[m]][free[[m]] != 0] <- est[m_idx]
+    start_idx <- tail(m_idx, n = 1)
+  }
+  mod
+}
+
+std_beta_est <- function(model_list, free_list = NULL, est = NULL) {
+  # The `est` argument is used to evaluate how changes in parameters
+  # affect the standardized estimates, and is used to obtain the
+  # derivatives for the delta method.
+  if (!is.null(est) && !is.null(free_list)) {
+    model_list <- .fill_matrix_list(model_list[c("beta", "psi")],
+                                    free = free_list[c("beta", "psi")],
+                                    est = est)
+  }
+  beta <- model_list$beta
+  psi <- model_list$psi
+  v_eta <- veta(beta, psi = psi)
+  s_eta <- sqrt(diag(v_eta))
+  inv_s_eta <- 1 / s_eta
+  diag(inv_s_eta) %*% beta %*% diag(s_eta)
+}
+
+# Function for combining free estimates into a vector
+.combine_est <- function(mod, free) {
+  out <- vector("list", length(free))
+  for (m in seq_along(free)) {
+    out[[m]] <- mod[[m]][free[[m]] != 0]
+  }
+  unlist(out)
+}
