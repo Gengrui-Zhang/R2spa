911994f501820e1c9f6249a16ef3ff91ebb6391e
Jimmy Zhang <gengruiz@usc.edu>
2022-12-02 04:20:20 -0800

    commit

--- R/tspa_plot.R
+++ R/tspa_plot.R
@@ -6,25 +6,25 @@
 #' @return A scatterplot and a residual plot between factor scores.
 #' @export
 #' @examples
-#' model <- '
-#' # latent variable definitions
-#' ind60 =~ x1 + x2 + x3
-#' dem60 =~ y1 + a*y2 + b*y3 + c*y4
-#' # regressions
-#' dem60 ~ ind60
-#' '
-#' fs_dat_ind60 <- get_fs(data = PoliticalDemocracy,
-#' model = "ind60 =~ x1 + x2 + x3")
-#' fs_dat_dem60 <- get_fs(data = PoliticalDemocracy,
-#'                        model = "dem60 =~ y1 + y2 + y3 + y4")
-#' fs_dat <- cbind(fs_dat_ind60, fs_dat_dem60)
-#'
-#' tspa_fit <- tspa(model = "dem60 ~ ind60",
-#' data = fs_dat,
-#' se = list(ind60 = 0.1213615, dem60 = 0.6756472))
-#' tspa_plot(tspa_fit)
+# model <- '
+# # latent variable definitions
+# ind60 =~ x1 + x2 + x3
+# dem60 =~ y1 + a*y2 + b*y3 + c*y4
+# # regressions
+# dem60 ~ ind60
+# '
+# fs_dat_ind60 <- get_fs(data = PoliticalDemocracy,
+# model = "ind60 =~ x1 + x2 + x3")
+# fs_dat_dem60 <- get_fs(data = PoliticalDemocracy,
+#                        model = "dem60 =~ y1 + y2 + y3 + y4")
+# fs_dat <- cbind(fs_dat_ind60, fs_dat_dem60)
+#
+# tspa_fit <- tspa(model = "dem60 ~ ind60",
+# data = fs_dat,
+# se = list(ind60 = 0.1213615, dem60 = 0.6756472))
+# tspa_plot(tspa_fit)
 
-tspa_plot <- function (tspa_fit, ...) {
+tspa_plot <- function (tspa_fit, ask = FALSE, ...) {
 
   fit_data <- parameterestimates(tspa_fit)
   latent_scores <- lavInspect(tspa_fit, what = "data")
@@ -38,29 +38,34 @@ tspa_plot <- function (tspa_fit, ...) {
 
     for (g in seq(length(df_latent_scores))) {
       for (i in seq(length(latent_dv))) {
-        par(mfrow=c(2,2))
         latent_model[[g]] <- lm(as.numeric(t(df_latent_scores[[g]][paste0("fs_", latent_dv[i])])) ~
                              as.numeric(t(df_latent_scores[[g]][paste0("fs_", latent_iv[i])])),
                            data = df_latent_scores[[g]])
 
+        if (ask == TRUE) {
+          invisible(readline(prompt = "Hit <Return> to see next plot: "))
+        }
         plot(latent_scores[[g]][ ,paste0("fs_", latent_iv[i])],
              latent_scores[[g]][ ,paste0("fs_", latent_dv[i])],
-             ylab = paste(latent_dv[i]),
-             xlab = paste(latent_iv[i]),
-             main = paste0("Scatterplot between ", latent_dv[i], " and\n ",
-                           latent_iv[i], " (factor scores; group ", g, ")"),
+             ylab = paste0("fs_", latent_iv[i]),
+             xlab = paste0("fs_", latent_dv[i]),
+             main = paste0("Scatterplot", " (group ", g, ")"),
              pch = 16,
              ...)
         abline(latent_model[[g]])
 
+        if (ask == TRUE) {
+          invisible(readline(prompt = "Hit <Return> to see next plot: "))
+        }
         df_latent_scores[[g]]$residuals <- latent_model[[g]]$residuals
         plot(latent_scores[[g]][ ,paste0("fs_", latent_iv[i])], df_latent_scores[[g]]$residuals,
-             ylab = latent_dv[i],
-             xlab = latent_iv[i],
+             ylab = "Residuals",
+             xlab = "Fitted values",
              main = paste0("Residual Plot (group ", g, ")"),
              pch = 18,
              ...)
-        abline(0, 0)
+        abline(0, 0,
+               lty = 3)
         lines(lowess(latent_scores[[g]][ ,paste0("fs_", latent_iv[i])], df_latent_scores[[g]]$residuals),
               col = "red")
       }
@@ -73,29 +78,34 @@ tspa_plot <- function (tspa_fit, ...) {
     latent_iv <- c(t(fit_data[which(fit_data$op == "~"),]["rhs"]))
 
     for (i in seq(length(latent_dv))) {
-      par(mfrow=c(2,2))
       latent_model <- lm(as.numeric(t(df_latent_scores[paste0("fs_", latent_dv[i])])) ~
                            as.numeric(t(df_latent_scores[paste0("fs_", latent_iv[i])])),
                          data = df_latent_scores)
 
+      if (ask == TRUE) {
+        invisible(readline(prompt = "Hit <Return> to see next plot: "))
+      }
       plot(latent_scores[ ,paste0("fs_", latent_iv[i])],
            latent_scores[ ,paste0("fs_", latent_dv[i])],
-           ylab = paste(latent_dv[i]),
-           xlab = paste(latent_iv[i]),
-           main = paste("Scatterplot between", latent_dv[i], "and\n",
-                        latent_iv[i], "(factor scores)"),
+           ylab = paste0("fs_", latent_dv[i]),
+           xlab = paste0("fs_", latent_dv[i]),
+           main = paste("Scatterplot"),
            pch = 16,
            ...)
       abline(latent_model)
 
+      if (ask == TRUE) {
+        invisible(readline(prompt = "Hit <Return> to see next plot: "))
+      }
       df_latent_scores$residuals <- latent_model$residuals
       plot(latent_scores[ ,paste0("fs_", latent_iv[i])], df_latent_scores$residuals,
-           ylab = latent_dv[i],
-           xlab = latent_iv[i],
+           ylab = "Residuals",
+           xlab = "Fitted values",
            main = "Residual Plot",
            pch = 18,
            ...)
-      abline(0, 0)
+      abline(0, 0,
+             lty = 3)
       lines(lowess(latent_scores[ ,paste0("fs_", latent_iv[i])], df_latent_scores$residuals),
             col = "red")
     }
