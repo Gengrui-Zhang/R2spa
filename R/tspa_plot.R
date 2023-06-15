0d6d4d12b4455408bc7f6b2843d8335e2da10fd9
Jimmy Zhang <gengruiz@usc.edu>
2023-06-15 02:10:36 -0700

    Addressed PR #41

--- R/tspa_plot.R
+++ R/tspa_plot.R
@@ -41,101 +41,126 @@
 #' se = list(ind60 = 0.1213615, dem60 = 0.6756472))
 #' tspa_plot(tspa_fit)
 
+# Helper function for the scatter plot
+# Comment: put it outside the main function
+plot_scatter <- function (fscores_df, iv, dv,
+                          # ab_slope, ab_intercept,
+                          g_num., g_name.,
+                          title., label_x., label_y.,
+                          ...) {
+
+  # Group name for multiple-group case
+  if (!is.null(g_name.)) {
+    iv_data <- as.data.frame(unname(fscores_df))[ ,paste0("fs_", iv)]
+    dv_data <- as.data.frame(unname(fscores_df))[ ,paste0("fs_", dv)]
+  } else {
+    iv_data <- fscores_df[ ,paste0("fs_", iv)]
+    dv_data <- fscores_df[ ,paste0("fs_", dv)]
+  }
+
+  # ylab
+  ylab <- c()
+  if (is.null(label_y.)) {
+    ylab <- paste0("fs_", dv)
+  } else {
+    ylab <- label_y.
+  }
+
+  # ylab
+  xlab <- c()
+  if (is.null(label_x.)) {
+    xlab <- paste0("fs_", iv)
+  } else {
+    xlab <- label_x.
+  }
+
+  # title
+  title <- c()
+  if (is.null(title.) & is.null(g_name.)) {
+    title <- paste0("Scatterplot")
+  } else if (is.null(title.) & !is.null(g_name.)) {
+    title <- paste0("Scatterplot", " (Group ", g_num., ": ", g_name., ")")
+  } else if (!is.null(title.) & is.null(g_name.)) {
+    title <- paste0(title.)
+  } else if (!is.null(title.) & !is.null(g_name.)) {
+    title <- paste0(title., " (Group ", g_num., ": ", g_name., ")")
+  }
+
+  plot(iv_data,
+       dv_data,
+       ylab = ylab,
+       xlab = xlab,
+       main = title,
+       pch = 16,
+       ...)
+  # abline(a = ab_intercept, b = ab_slope, lwd = 2)
+}
+
+# Helper function for the residual plot
+plot_residual <- function (fscores_df, iv, dv,
+                           tspa_fit. = tspa_fit,
+                           g_num. = g_num, g_name. = g_name,
+                           ...) {
+
+  if (!is.null(g_name.)) {
+    iv_data <- as.data.frame(unname(fscores_df))[ ,paste0("fs_", iv)]
+    dv_data <- as.data.frame(unname(fscores_df))[ ,paste0("fs_", dv)]
+  } else {
+    iv_data <- fscores_df[ ,paste0("fs_", iv)]
+    dv_data <- fscores_df[ ,paste0("fs_", dv)]
+  }
+
+  predicted_y <- lavaan::lavPredictY(tspa_fit., ynames = paste0("fs_", dv), xnames = paste0("fs_", iv))
+
+  if (!is.null(g_name.)) {
+    resid <- dv_data -  predicted_y[which(names(fscores_df) == predicted_y[ ,2]), ][,1]
+  } else {
+    resid <- dv_data -  predicted_y
+  }
+
+  # title
+  title <- c()
+  if (!is.null(g_name.)) {
+    title <- paste0("Residual Plot", " (Group ", g_num., ": ", g_name., ")")
+  } else {
+    title <- paste0("Residual Plot")
+  }
+
+  plot(iv_data,
+       resid,
+       ylab = paste0("Residuals: ", dv),
+       xlab = paste0("Fitted values: ", iv),
+       main = title,
+       pch = 18,
+       ...)
+}
+
 tspa_plot <- function(tspa_fit,
                       title = NULL,
                       label_x = NULL,
                       label_y = NULL,
                       abbreviation = TRUE,
-                      fscore_type = NULL,
+                      fscores_type = c("original", "lavaan"),
                       ask = FALSE,
                       ...) {
 
-  fit_pars <- lavaan::parameterestimates(tspa_fit) # parameter estimates
-  regression_fit <- fit_pars[which(fit_pars$op == "~"),] # path coefficients
-  latent_dv <- unique(c(unname(t(regression_fit["lhs"])))) # Extract DV
-  latent_iv <- unique(c(unname(t(regression_fit["rhs"])))) # Extract IV
-  fscores_func <- lavaan::lavInspect(tspa_fit, what = "data") # factor scores from `get_fs`
-  fscores_est <- lavaan::lavPredict(tspa_fit) # factor scores from estimation using `lavPredict`
+    fit_pars <- lavaan::parameterestimates(tspa_fit) # parameter estimates
+    regression_fit <- fit_pars[which(fit_pars$op == "~"),] # path coefficients
+    latent_dv <- unique(c(unname(t(regression_fit["lhs"])))) # Extract DV
+    latent_iv <- unique(c(unname(t(regression_fit["rhs"])))) # Extract IV
+    fscores_type <- match.arg(fscores_type) # Specify the type of factor scores
+
+    reg_pairs <- names(coef(tspa_fit)[names(coef(tspa_fit)) %in%
+                                  apply(expand.grid(latent_dv, latent_iv), 1, paste, collapse ="~")])
 
     # Type of scatterplot
-    if (is.null(fscore_type)) {
-      fscores <- fscores_func
+    if (fscores_type == "original") {
+      fscores <- lavaan::lavInspect(tspa_fit, what = "data") # factor scores from `get_fs`
     } else {
-      fscores <- fscore_est
+      fscores <- lavaan::lavPredict(tspa_fit) # factor scores from estimation using `lavPredict`
     }
 
-    # Helper function for scatter plot
-    # Comment: put it outside the main function
-    plot_scatter <- function (fscores_df, iv, dv,
-                              # ab_slope, ab_intercept,
-                              g_num = NULL, g_name = NULL, ...) {
-
-      # Ask for next plot
-      if (ask == TRUE) {
-        invisible(readline(prompt = "Hit <Return> to see next plot: "))
-      }
-
-      if (!is.null(g_name)) {
-        iv_data <- as.data.frame(unname(fscores_df))[ ,paste0("fs_", iv)]
-        dv_data <- as.data.frame(unname(fscores_df))[ ,paste0("fs_", dv)]
-      } else {
-        iv_data <- fscores_df[ ,paste0("fs_", iv)]
-        dv_data <- fscores_df[ ,paste0("fs_", dv)]
-      }
-
-      plot(iv_data,
-           dv_data,
-           ylab = ifelse(is.null(label_y), paste0("fs_", dv),
-                         ifelse(length(label_y) > 1, label_y[i], label_y)),
-           xlab = ifelse(is.null(label_x), paste0("fs_", iv),
-                         ifelse(length(label_x) > 1, label_x[i], label_x)),
-           main = ifelse(!is.null(g_name),
-                         ifelse(is.null(title),
-                                paste0("Scatterplot", " (Group ", g_num, ": ", g_name, ")"),
-                                ifelse(length(title) > 1, title[i], title)),
-                         ifelse(is.null(title),
-                                paste0("Scatterplot"),
-                                ifelse(length(title) > 1, title[i], title))),
-           pch = 16,
-           ...)
-      # abline(a = ab_intercept, b = ab_slope, lwd = 2)
-    }
-
-    plot_residual <- function (fscores_df, iv, dv,
-                               g_num = NULL, g_name = NULL, ...) {
-
-      # Ask for next plot
-      if (ask == TRUE) {
-        invisible(readline(prompt = "Hit <Return> to see next plot: "))
-      }
-
-      if (!is.null(g_name)) {
-        iv_data <- as.data.frame(unname(fscores_df))[ ,paste0("fs_", iv)]
-        dv_data <- as.data.frame(unname(fscores_df))[ ,paste0("fs_", dv)]
-      } else {
-        iv_data <- fscores_df[ ,paste0("fs_", iv)]
-        dv_data <- fscores_df[ ,paste0("fs_", dv)]
-      }
-
-      predicted_y <- lavaan::lavPredictY(tspa_fit, ynames = paste0("fs_", dv), xnames = paste0("fs_", iv))
-      if (!is.null(g_name)) {
-        resid <- dv_data -  predicted_y[which(names(fscores_df) == predicted_y[ ,2]), ][,1]
-      } else {
-        resid <- dv_data -  predicted_y
-      }
-
-      plot(iv_data,
-           resid,
-           ylab = paste0("Residuals: ", dv),
-           xlab = paste0("Fitted values: ", iv),
-           main = ifelse(!is.null(g_name),
-                         paste0("Residual Plot: ", " (Group ", g_num, ": ", g_name, ")"),
-                         paste0("Residual Plot")),
-           pch = 18,
-           ...)
-    }
-
-    # Examine multi-group sample
+    # Determine multi-group sample
     if (is.list(fscores)) {
 
       # Ask for abbreviation
@@ -147,7 +172,7 @@ tspa_plot <- function(tspa_fit,
         }
 
       # Prepare for abline
-      df_latent_scores <- lapply(fscores, data.frame)
+      fscores <- lapply(fscores, data.frame)
       # slope <- coef(tspa_fit)[grepl(apply(expand.grid(latent_dv, latent_iv), 1, paste, collapse="~"),
       #                               names(coef(tspa_fit)))]
       # fs_means <- matrix(unlist(lapply(df_latent_scores, apply, 2, mean, na.rm = T)),
@@ -156,25 +181,42 @@ tspa_plot <- function(tspa_fit,
       # intercept <- fs_means[,2] - fs_means[,1]*slope
 
       for (g in seq_len(length(g_names))) {
+        for (i in seq_len(length(reg_pairs))) {
+        # Ask for next plot
+          if (ask == TRUE) {
+            invisible(readline(prompt = "Hit <Return> to see next plot: "))
+          }
 
-        # Scatterplots
-        plot_scatter(fscores_df = df_latent_scores[g],
-                     iv = latent_iv,
-                     dv = latent_dv,
-                     # ab_slope = slope[g],
-                     # ab_intercept = intercept[g],
-                     g_num = g,
-                     g_name = g_names[g],
-                     ...)
-
-        # Residual Plots
-        plot_residual(fscores_df = df_latent_scores[g],
-                       iv = latent_iv,
-                       dv = latent_dv,
-                       g_num = g,
-                       g_name = g_names[g],
+          # Scatterplot
+          plot_scatter(fscores_df = fscores[g],
+                       iv = unlist(strsplit(reg_pairs[i], split = "~"))[2],
+                       dv = unlist(strsplit(reg_pairs[i], split = "~"))[1],
+                       # ab_slope = slope[g],
+                       # ab_intercept = intercept[g],
+                       g_num. = g,
+                       g_name. = g_names[g],
+                       title. = title[i],
+                       label_x. = label_x[i],
+                       label_y. = label_y[i],
                        ...)
+
+          # Ask for next plot
+          if (ask == TRUE) {
+            invisible(readline(prompt = "Hit <Return> to see next plot: "))
+          }
+
+          # Residual Plot
+          plot_residual(fscores_df = fscores[g],
+                         iv = unlist(strsplit(reg_pairs[i], split = "~"))[2],
+                         dv = unlist(strsplit(reg_pairs[i], split = "~"))[1],
+                         tspa_fit. = tspa_fit,
+                         g_num = g,
+                         g_name = g_names[g],
+                         ...)
+          }
       }
+
+    # Examine single-group example
     } else {
 
       # Prepare for abline
@@ -189,23 +231,39 @@ tspa_plot <- function(tspa_fit,
       # }
       # names(intercept) <- names(slope)
 
-      reg_pairs <- coef(tspa_fit)[names(coef(tspa_fit)) %in%
-                                   apply(expand.grid(latent_dv, latent_iv), 1, paste, collapse ="~")]
-
       for (i in seq_len(length(reg_pairs))) {
+
+        # Ask for next plot
+        if (ask == TRUE) {
+          invisible(readline(prompt = "Hit <Return> to see next plot: "))
+        }
 
         # Scatterplots
         plot_scatter(fscores_df = fscores,
-                     iv = unlist(strsplit(names(reg_pairs[i]), split = "~"))[2],
-                     dv = unlist(strsplit(names(reg_pairs[i]), split = "~"))[1],
+                     iv = unlist(strsplit(reg_pairs[i], split = "~"))[2],
+                     dv = unlist(strsplit(reg_pairs[i], split = "~"))[1],
                      # ab_slope = slope[i],
                      # ab_intercept = intercept[i],
-                     g_num = i)
+                     g_num. = NULL,
+                     g_name. = NULL,
+                     title. = title[i],
+                     label_x. = label_x[i],
+                     label_y. = label_y[i],
+                     ...)
+
+        # Ask for next plot
+        if (ask == TRUE) {
+          invisible(readline(prompt = "Hit <Return> to see next plot: "))
+        }
 
         # Resiudal Plots
         plot_residual(fscores_df = fscores,
-                      iv = unlist(strsplit(names(reg_pairs[i]), split = "~"))[2],
-                      dv = unlist(strsplit(names(reg_pairs[i]), split = "~"))[1])
+                      iv = unlist(strsplit(reg_pairs[i], split = "~"))[2],
+                      dv = unlist(strsplit(reg_pairs[i], split = "~"))[1],
+                      tspa_fit. = tspa_fit,
+                      g_num = NULL,
+                      g_name = NULL,
+                      ...)
       }
     }
 }
